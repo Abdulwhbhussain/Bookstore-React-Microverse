c61dd6faa3e8b966d9b764be66ddd99466bfbb29
WHB901 <abdulwhbhussain@gmail.com>
2023-06-08 09:28:25 +0500

    Setup of react components2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
 
 function App() {
   return (
-    <div className="App">
-      Wahab
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
